eddc88006fc0571b499123683ffd90cf3cdb6295
meatball <69751659+meatball133@users.noreply.github.com>
2023-10-16 17:35:56 +0000

    fix

--- src/cli.cr
+++ src/cli.cr
@@ -38,10 +38,12 @@ OptionParser.parse do |parser|
   parser.on "-d DIRECTORY", "--directory DIRECTORY", "Directory to parse" do |directory|
     puts "Parsing directory #{directory}"
     path = Path.new(directory)
-    if options[:specified_files].empty?
-      representer.parse_folder(path)
-    else
-      representer.parse_folder(path, options[:specified_files])
+    if specified_files = options[:specified_files]
+      if specified_files.empty?
+        representer.parse_folder(path)
+      else
+        representer.parse_folder(path, options[:specified_files])
+      end
     end
     puts "Done!"
   end
